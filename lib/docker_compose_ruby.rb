e241c260e5d927006040721a1ad655de36c69944
Mitch Eaton <meaton@mitchs-mbp.us.ad.westfield.com>
2016-02-23 15:30:36 -0800

    adding initial module

--- lib/docker_compose_ruby.rb
+++ lib/docker_compose_ruby.rb
@@ -1,5 +1,38 @@
 require "docker_compose_ruby/version"
+require 'json'
+require 'yaml'
+require 'pry-nav'
 
-module DockerComposeRuby
-  # Your code goes here...
+module DockerCompose
+  class << self
+    attr_accessor :project_name, :yaml_path
+
+    def set_yaml_path(path)
+      @yaml_path = "-f #{path}"
+    end
+
+    def set_project_name(name)
+      @project_name = "-p #{name}"
+    end
+
+    def up
+      compose('up')
+    end
+
+    def build
+      compose('build')
+    end
+
+    def kill(contianer)
+      compose('kill',container)
+    end
+
+    def compose(command, options = {})
+      `docker-compose #{@yaml_path} #{@project_name} #{command}`
+    end
+
+    def version
+      compose('-v')
+    end
+  end
 end
