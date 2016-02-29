92f78d8706b97c8c2039e96b72b29b68ceefb67c
Mitch Eaton <meaton@mitchs-mbp.us.ad.westfield.com>
2016-02-29 12:14:17 -0800

    adding documentation

--- lib/docker_compose_ruby.rb
+++ lib/docker_compose_ruby.rb
@@ -7,36 +7,91 @@ module DockerCompose
   class << self
     attr_accessor :project_name, :yaml_path, :yaml
 
+
+    # Set Path to Docker Compose Yaml File.
+    #
+    # @param path [String]
+    # @return [String]
+    #
+    # @example
+    #   DockerCompose.set_yaml_path('/Users/test/projects/docker-compose.yml')
     def set_yaml_path(path)
       @yaml_path = "-f #{path}"
       @yaml = YAML.load_file(path)
     end
 
+    # Set Specific compose project name.
+    #
+    # @param name [String]
+    # @return [String]
+    #
+    # @example
+    #   DockerCompose.set_project_name('test_project')
     def set_project_name(name)
       @project_name = "-p #{name}"
       name
     end
 
+    # Ups the set of containers listed in your compose file.
+    #
+    # @param options [Hash]
+    # @return [String]
+    #
+    # @example
+    #   DockerCompose.up
     def up(options = {})
       options = nil if options.empty?
       compose('up',options)
     end
 
+    # Deletes the set of containers listed in your compose file.
+    #
+    # @param options [Hash]
+    # @return [String]
+    #
+    # @example
+    #   DockerCompose.delete
     def delete(options = {})
       options = nil if options.empty?
       compose('delete',options)
     end
 
+    # Stops the set of containers listed in your compose file.
+    #
+    # @param options [Hash]
+    # @return [String]
+    #
+    # @example
+    #   DockerCompose.stop
     def stop(options = {})
       options = nil if options.empty?
       compose('stop', options)
     end
 
+    # Starts the set of containers listed in your compose file.
+    #
+    # @param options [Hash]
+    # @return [String]
+    #
+    # @example
+    #   DockerCompose.up
     def start(options = {})
       options = nil if options.empty?
       compose('start', options)
     end
 
+    # Displays the current version of Docker compose
+    #
+    # @param options [Hash]
+    # @return [String]
+    #
+    # @example
+    #   DockerCompose.version
+    def version
+      compose('-v')
+    end
+
+    private
     def compose(command, options)
       case command
       when 'up'
@@ -44,13 +99,9 @@ module DockerCompose
       when 'delete'
         `docker-compose #{options} #{@yaml_path} #{@project_name} kill`
         `docker-compose #{options} #{@yaml_path} #{@project_name} rm -f`
-      when 'stop', 'start'
+      when 'stop', 'start', 'version'
         `docker-compose #{options} #{@yaml_path} #{@project_name} #{command}`
       end
-    end
-
-    def version
-      compose('-v')
     end
   end
 end
